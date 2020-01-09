1cb84b7b31dff48581fb6913a0d73e73b5ae3b0c
Bauter <bauter09@gmail.com>
2020-01-09 00:12:02 -0600

    basic html layout created with button styling, and JS start button function set up (w/o timer)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,11 @@
 $(document).ready(() => {
 
+    // start button clicked, start div disappears, question div pops up, timer starts
+    $('#start-btn').on('click', () => {
+        $('#start').empty();
+        $('#questions').show();
+        // timer start
+    });
 
 
 
@@ -31,5 +37,4 @@ $(document).ready(() => {
 
 
 
-    
 })
